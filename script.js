8a5b7f40dcd5c8816ef31d613a2f8376e3d2c616
bazza <davide.bazzanella-2@studenti.unitn.it>
2016-11-16 14:09:35 +0100

    first version limit

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var leftArray = [];
 var rightArray = [];
 var leftInput;
 var rightInput;
+var limit=30;
+var lock=0;
 var table = document.getElementById("table");
 
 function addRow(element1, element2){
@@ -38,30 +40,39 @@ function takeElementFromTable(){
 function takeElementFromInputTable(){
     var Ftable = document.getElementById("inputTable");
     console.log("Son dentro takeElementFromInputTable")
-    leftInput = document.getElementById('1').value;
-    rightInput = document.getElementById('2').value;
+    leftInput = document.getElementById('oggetto').value;
+    rightInput = document.getElementById('quantita').value;
 }
 
 function addElementToTable(){
-    takeElementFromInputTable();
-    takeElementFromTable();
-    var Ftable = document.getElementById("table");
-    var rowLength = Ftable.rows.length;
-    console.log("RowLength: "+rowLength);
-    console.log("LeftInput: "+leftInput);
-    console.log("RightInput: "+rightInput);
-    console.log("LeftArray[1]: "+leftArray[1]);
-    var mark=0;
-    for(i=1;i<rowLength;i++){
-        console.log("Sono nel ciclo i: "+i);
-            if(leftArray[i]==leftInput){
-                rightArray[i] = rightArray[i]+parseInt(rightInput);
-                mark = 1;
-                cancelAndRedo(leftArray[i],rightArray[i],i);
-            }
+    if(lock==1) alert("Hai sforato il magazzino, non verranno accettati altri componenti");
+    else {
+        takeElementFromInputTable();
+        takeElementFromTable();
+        var tot = sumOfElements(rightArray);
+        if(testLimit(tot,limit)==1){
+            alert("Hai sforato il magazzino, non verranno accettati altri componenti");
+            lock = 1;
+        } 
+        var Ftable = document.getElementById("table");
+        var rowLength = Ftable.rows.length;
+        console.log("RowLength: "+rowLength);
+        console.log("LeftInput: "+leftInput);
+        console.log("RightInput: "+rightInput);
+        console.log("LeftArray[1]: "+leftArray[1]);
+        var mark=0;
+        for(i=1;i<rowLength;i++){
+            console.log("Sono nel ciclo i: "+i);
+                if(leftArray[i]==leftInput){
+                    rightArray[i] = rightArray[i]+parseInt(rightInput);
+                    mark = 1;
+                    cancelAndRedo(leftArray[i],rightArray[i],i);
+                }
             
-        }
-    if(mark==0) addRow(leftInput,rightInput);
+            }
+        if(mark==0) addRow(leftInput,rightInput);
+        
+    } 
 }
 
 function cancelAndRedo(element1,element2,point){
@@ -72,6 +83,24 @@ function cancelAndRedo(element1,element2,point){
     var cell2 = row.insertCell(1);
     // Add some text to the new cells:
     cell1.innerHTML = element1;
-    cell2.innerHTML = element2;
-    
+    cell2.innerHTML = element2; 
+}
+
+
+function setLimit(){
+    var x = document.getElementById("inputLimit");
+    limit = x;
+}
+
+function testLimit(parameter1, parameter2){
+    var res=0;
+    if(parameter1>parameter2) res=1;
+    return res;
+}
+
+function sumOfElements(array){
+    var sum=0;
+    for(i=0;i<array.length;i++)
+        sum += array[i];
+    return sum;
 }
